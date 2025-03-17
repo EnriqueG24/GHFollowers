d4050a9a2cc99f2dda5f4885014b7fd576eb7999
Enrique <EGTech24@gmail.com>
2025-03-17 15:06:38 -0700

    Initial Setup

---
+++ GHFollowersTests/GHFollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersTests.swift
+//  GHFollowersTests
+//
+//  Created by Enrique Gongora on 3/17/25.
+//
+
+import Testing
+@testable import GHFollowers
+
+struct GHFollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
