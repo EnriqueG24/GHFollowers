03558bdd6cc02f2f551fbb158685f0d92b900e98
Enrique <EGTech24@gmail.com>
2025-03-21 13:33:17 -0700

    Created Followers and User Models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Enrique Gongora on 3/20/25.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
