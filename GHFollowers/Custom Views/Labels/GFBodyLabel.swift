6bc1b802e6d4e1e9dc267957b7f711082d802203
Enrique <EGTech24@gmail.com>
2025-03-30 19:00:28 -0700

    Delegate and protocol refactor

--- GHFollowers/Custom Views/Labels/GFBodyLabel.swift
+++ GHFollowers/Custom Views/Labels/GFBodyLabel.swift
@@ -26,6 +26,8 @@ class GFBodyLabel: UILabel {
     private func configure() {
         textColor = .secondaryLabel
         font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontForContentSizeCategory = true
+        adjustsFontSizeToFitWidth = true
         adjustsFontSizeToFitWidth = true
         minimumScaleFactor = 0.75
         lineBreakMode = .byWordWrapping
