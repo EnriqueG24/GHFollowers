b2eab54153abbad80fb5c4aecc88f7760d481e6d
Enrique <EGTech24@gmail.com>
2025-03-30 20:00:00 -0700

    I’ve implemented a scroll view in the User Info screen to ensure that smaller devices like the iPhone SE can view all the content.

--- GHFollowers/Extensions/UIView + Ext.swift
+++ GHFollowers/Extensions/UIView + Ext.swift
@@ -14,4 +14,33 @@ extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { addSubview($0) }
     }
+    
+    /// Pins the edges of the view to the edges of its superview.
+    ///
+    /// This helper method quickly anchors a view to all edges (top, leading, trailing, bottom)
+    /// of its superview using Auto Layout constraints. It automatically sets
+    /// `translatesAutoresizingMaskIntoConstraints` to `false` for you.
+    ///
+    /// - Parameter superview: The superview to which the edges will be pinned.
+    /// - Important: The view must already be added as a subview of the specified superview
+    ///   before calling this method, otherwise it will result in a runtime error.
+    ///
+    /// # Example
+    /// ```
+    /// let childView = UIView()
+    /// parentView.addSubview(childView)
+    /// childView.pinToEdges(of: parentView)
+    /// ```
+    ///
+    /// - Note: This method uses zero padding between the views.
+    func pinToEdges(of superview: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+        ])
+    }
 }
